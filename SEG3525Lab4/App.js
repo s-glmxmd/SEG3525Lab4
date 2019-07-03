800e2b58cb9fc096bf9f64ae0e42c3a9381e644b
Alexander <alex202ap@gmail.com>
2019-07-03 14:45:43 -0400

    Work on navigation bar

--- SEG3525Lab4/App.js
+++ SEG3525Lab4/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationBar from 'react-native-navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ComponentWithNavigationBar/>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This works!</Text>
     </View>
   );
 }
@@ -16,4 +19,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navBar: {
+    flex: 0,
+    backgroundColor: '#fff3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top:0,
+    maxHeight: 20,
+    padding: 10,
+    paddingTop: 0,
+
+  },
+
 });
+
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};
+ 
+const titleConfig = {
+  title: 'Hello, world',
+};
+
+function ComponentWithNavigationBar() {
+  return (
+    <View style={styles.navBar}>
+      <NavigationBar
+        title={titleConfig}
+        rightButton={rightButtonConfig}
+      />
+    </View>
+  );
+}
