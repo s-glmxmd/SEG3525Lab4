cdd9b3b7ed2c7aab0bf310869b346f417d78af8c
Alexander <alex202ap@gmail.com>
2019-07-04 03:05:59 -0400

    Added Navigation bar, homepage and Restaurant screen

--- SEG3525Lab4/App.js
+++ SEG3525Lab4/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 import Payment from './Pages/Payment';
+import HomePage from './Pages/HomePage';
+import Restaurants from './Pages/Restaurants';
 import Home from './components/Home'
 import Search from './components/Search'
 import Navbar from './components/navbar'
 
-export function App() {
-  return (
-    <View style={styles.container}>
-      <Navbar/>
-      <Home/>
-      <Text>This works!</Text>
-    </View>
-  );
-}
+// export function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Navbar/>
+//       <Home/>
+//       <Text>This works!</Text>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomePage},
+  Profile: {screen: Restaurants},
 });
 
-export default Payment;
+const App = createAppContainer(MainNavigator);
+
+
+export default App;
---
+++ SEG3525Lab4/Pages/HomePage.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,Image,
+  View, 
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { NavigationBar } from 'navigationbar-react-native';
+
+
+const ComponentLeft = () => {
+  return(
+    <View style={{ flex: 1, alignItems: 'flex-start'}} >
+       <TouchableOpacity style={ {justifyContent:'center', flexDirection: 'row'}}>
+        <Text style={{ color: 'white', marginLeft: 20, }}>Back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+ 
+const ComponentCenter = () => {
+  return(
+    <View style={{ flex: 1, }}>
+       <Text style={{color: 'white', alignSelf: 'center' }}>Home</Text>
+    </View>
+  );
+};
+ 
+const ComponentRight = () => {
+  return(
+    <View style={{ flex: 1, alignItems: 'flex-end', }}>
+      <TouchableOpacity>
+      {/* <Button
+        title="Next"
+        onPress={() => navigate('Profile')}
+      /> */}
+        <Text style={{ color: 'white', marginRight: 20, }}> Next </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  welcome: {
+    flex: 0,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30,
+  },
+});
+
+class HomePage extends Component{
+  // static navigationOptions = {
+  //   title: 'Welcome',
+  // };
+
+
+
+
+      render() {
+        // const {navigate} = this.props.navigation;
+          return (
+            <View style={styles.container}>
+              <NavigationBar 
+                //componentLeft     = { () =>  <ComponentLeft />   }
+                componentCenter   = { () =>  <ComponentCenter /> }
+                componentRight    = { () =>  <ComponentRight />  }
+                navigationBarStyle= {{ backgroundColor: '#215e79' }}
+                statusBarStyle    = {{ barStyle: 'light-content', backgroundColor: '#215e79' }}
+                />
+              <View style={styles.welcome}>
+                <Text>Welcome to Restaurant App!</Text>
+              </View>
+            </View>
+            
+          );
+      }
+}
+
+
+
+export default HomePage;
